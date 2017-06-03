11c9df349f660ac76116fcdbcd72bece6dc41988
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-06-02 21:13:40 -0700

    Add decoding from text strings

--- test/decoding.js
+++ test/decoding.js
@@ -3,6 +3,24 @@ const expect = require('chai').expect
 const base32 = require('../lib/index.js')
 
 describe('decoding', function () {
+  describe('decode', function () {
+    it('decodes a base32-encoded text string', function () {
+      expect(base32.decode('A1850M2G')).to.equal('PPPPP')
+    })
+
+    it('handles lowercase', function () {
+      expect(base32.decode('a1850m2g')).to.equal('PPPPP')
+    })
+
+    it('handles substitutions', function () {
+      expect(base32.decode('Al85OM2G')).to.equal('PPPPP')
+    })
+
+    it('handles longer length strings', function () {
+      expect(base32.decode('A1850M2GAl85OM2G')).to.equal('PPPPPPPPPP')
+    })
+  })
+
   describe('decodeChunk', function () {
     it('decodes two symbols', function () {
       expect(base32.decodeChunk('A1')).to.equal('P')
